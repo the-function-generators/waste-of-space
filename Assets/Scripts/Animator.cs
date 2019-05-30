92bd62b7ae86043ed46e326b2fe2f57534c3ccfa
hnbrake <hnbrakemun@gmail.com>
2019-05-29 22:06:42 -0230

    Character can move  Player can look Camera third person now  But major glitches with looking while moving.
    Character can move
    Player can look
    Camera third person now
    
    But major glitches with looking while moving.

---
+++ Assets/Scripts/Animator.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animation anim;
+
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S))
+        {
+           
+            Debug.Log("Walking");
+            anim.Play("FallenAngle_Walk");
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D) || Input.GetKeyUp(KeyCode.S))
+        {
+            anim.Stop("FallenAngle_Walk");
+        }
+
+    }
+}
